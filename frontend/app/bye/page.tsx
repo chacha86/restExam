056b7eaf79cce722ea4d081dc4244aa8da1211d0
woooyoung <axdsw121@gmail.com>
2024-12-18 20:32:01 +0900

    client side fetch & backedn cors setting

---
+++ frontend/app/bye/page.tsx
@@ -0,0 +1,4 @@
+export default function Bye() {
+    return "byebye";
+}
+  
--- src/main/java/com/example/restexam/controller/PostController.java
+++ src/main/java/com/example/restexam/controller/PostController.java
@@ -4,15 +4,14 @@ import com.example.restexam.entity.Post;
 import com.example.restexam.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@RestController // 모든 메서드의 리턴을 문자 그대로 전달. template 사용 X -> JSON 문자열로 전달
 @RequiredArgsConstructor
 @RequestMapping("/posts")
+@CrossOrigin("http://localhost:3000") // localhost:3000 번에 대해서는 데이터를 허용해줘라.
 public class PostController {
 
     private final PostService postService;
@@ -21,5 +20,4 @@ public class PostController {
     public List<Post> getList() {
         return postService.getList();
     }
-
 }
